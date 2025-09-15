e3ca0418f5b773d6d669bd46697ff1345c9ac144
GitHub Action <action@github.com>
2025-09-15 08:08:36 +0000

    Add/Update convstorelib for conv-v986d06b

--- Package.swift
+++ Package.swift
@@ -8,19 +8,19 @@ let package = Package(
     .macOS(.v12)
   ],
   products: [
-    .library(name: "SandboxSDK", targets: ["SandboxSDK"])
-   ,.library(name: "convstorelib", targets: ["convstorelib"])
+    .library(name: "SandboxSDK", targets: ["SandboxSDK"]),
+    .library(name: "convstorelib", targets: ["convstorelib"])
   ],
   targets: [
     .binaryTarget(
       name: "SandboxSDK",
       url: "https://github.com/Geeksfino/finclip-neuron/releases/download/sdk-ve5de24f/SandboxSDK.xcframework.zip",
       checksum: "3f40fd85902bbe8268d30572fca43afd129c3a0f50b4a46b8d0d20540f58ec94"
-    )
-   ,.binaryTarget(
+    ),
+    .binaryTarget(
       name: "convstorelib",
-      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/sdk-ve5de24f/convstorelib.xcframework.zip",
-      checksum: "d2df10250ef1bd6e85a4c2b2df241d25e23e478e7d5e5fae0ff2fe7e6da4d822"
+      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/conv-v986d06b/convstorelib.xcframework.zip",
+      checksum: "cf467acabdd7036e1543c48994e7a50cdc11374dac19206b12453b61b20199fd"
     )
   ]
 )
