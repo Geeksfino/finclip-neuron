34a13b02612fd62bfbf08335005f66e506517ffc
GitHub Action <action@github.com>
2025-09-15 04:44:33 +0000

    Update Package.swift for sdk-v9a855e6

--- Package.swift
+++ Package.swift
@@ -2,7 +2,6 @@
 import PackageDescription
 
 // Distribution package for SandboxSDK binary XCFramework (iOS + macOS)
-// Update the URL and checksum after CI publishes a new release.
 let package = Package(
   name: "SandboxSDK",
   platforms: [
@@ -15,8 +14,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "SandboxSDK",
-      url: "https://github.com/Geeksfino/finclip-sandbox/releases/download/sdk-vREPLACE_ME/SandboxSDK.xcframework.zip",
-      checksum: "REPLACE_ME_CHECKSUM"
+      url: "https://github.com/Geeksfino/finclip-sandbox/releases/download/sdk-v9a855e6/SandboxSDK.xcframework.zip",
+      checksum: "a6502f205b112a07bc0bcec6a530f278736ac62960f333392eb8ff80f89ab907"
     )
   ]
 )
