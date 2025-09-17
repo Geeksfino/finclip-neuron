09fe1b548ddeb25fc11c9c0261babaff392d7ab4
GitHub Action <action@github.com>
2025-09-17 23:35:37 +0000

    Update convstorelib for conv-v32fa11d-swift6_0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
@@ -20,8 +20,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "convstorelib",
-      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/conv-v52d4562-swift6_0/convstorelib.xcframework.zip",
-      checksum: "60891f2206e45defa3def4e257d349ec53d25e5c39c8a0b76a500a7c363bec77"
+      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/conv-v32fa11d-swift6_0/convstorelib.xcframework.zip",
+      checksum: "e1cdf3ad5bb8b39442c92ecf12d4aadbd1f3e800977cbcdb447c3d8025e4c0bd"
     )
    ,
     .binaryTarget(
