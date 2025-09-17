d51fb3fa718140df25c94f6eb5260563dcb36c05
GitHub Action <action@github.com>
2025-09-17 12:25:44 +0000

    Add/Update NeuronKit for neuronkit-v191947f

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
   ],
   products: [
     .library(name: "SandboxSDK", targets: ["SandboxSDK"]),
-    .library(name: "convstorelib", targets: ["convstorelib"])
+    .library(name: "convstorelib", targets: ["convstorelib"]),
+    .library(name: "NeuronKit", targets: ["NeuronKit"])
   ],
   targets: [
     .binaryTarget(
@@ -21,6 +22,11 @@ let package = Package(
       name: "convstorelib",
       url: "https://github.com/Geeksfino/finclip-neuron/releases/download/sdk-v6497964/convstorelib.xcframework.zip",
       checksum: "f14151ae5af48224968df86ea531491dd959aad362a3a13b41f93ef74619d056"
+    ),
+    .binaryTarget(
+      name: "NeuronKit",
+      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/neuronkit-v191947f/NeuronKit.xcframework.zip",
+      checksum: "0dd1a77f623b9f37d2287ed16520a95a4be4ab794598b70fa152d720c088797a"
     )
   ]
 )
