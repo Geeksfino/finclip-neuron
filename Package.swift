b2a7df9646fd30543a52b1b6edbfefc1fdf9d909
GitHub Action <action@github.com>
2025-09-19 01:12:41 +0000

    Update SandboxSDK for sdk-vc90ac18-swift6_0

--- Package.swift
+++ Package.swift
@@ -12,14 +12,16 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "SandboxSDK",
-      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/sdk-v0661965-swift6_0/SandboxSDK.xcframework.zip",
-      checksum: "056d3c2118dc9683117fe49843443c7ff7ea9dfa6f37c53cfa450c106b3bd95f"
-    ),
+      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/sdk-vc90ac18-swift6_0/SandboxSDK.xcframework.zip",
+      checksum: "7376c401868ce2b97d7edd2b5cffef5e1ae3aa35a1f46fb7e5d7f23d8ea54e6b"
+    )
+   ,
     .binaryTarget(
       name: "convstorelib",
       url: "https://github.com/Geeksfino/finclip-neuron/releases/download/conv-v6ca80f6-swift6_0/convstorelib.xcframework.zip",
       checksum: "03861a0bbfecdd66f2063976bcf5c2a2648c24f644c6b4790c26ac1955105543"
-    ),
+    )
+   ,
     .binaryTarget(
       name: "NeuronKit",
       url: "https://github.com/Geeksfino/finclip-neuron/releases/download/neuronkit-vc4c459e-swift6_0/NeuronKit.xcframework.zip",
