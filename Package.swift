c09ff2f87ae2cba4095ee5d4ee5d6dea996c1cc2
GitHub Action <action@github.com>
2025-09-25 07:03:51 +0000

    Update NeuronKit for neuronkit-vb79e0a5-swift6_0

--- Package.swift
+++ Package.swift
@@ -14,18 +14,16 @@ let package = Package(
       name: "SandboxSDK",
       url: "https://github.com/Geeksfino/finclip-neuron/releases/download/sdk-vf64da07-swift6_0/SandboxSDK.xcframework.zip",
       checksum: "ca5db62a860bb421bdefe6ceed26bdb8efa00789f65e838ddd2e3508e277cbbc"
-    )
-   ,
+    ),
     .binaryTarget(
       name: "convstorelib",
       url: "https://github.com/Geeksfino/finclip-neuron/releases/download/conv-v1a27ae4-swift6_0/convstorelib.xcframework.zip",
       checksum: "bdf13010debfd9463d1fd14f32861554f99fb86e503eaac5ea6fe0c962a07d66"
-    )
-   ,
+    ),
     .binaryTarget(
       name: "NeuronKit",
-      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/neuronkit-v1e05524-swift6_0/NeuronKit.xcframework.zip",
-      checksum: "b02c517e57397042de992c1838c128c825b5be624995264881b97057abb5ee9a"
+      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/neuronkit-vb79e0a5-swift6_0/NeuronKit.xcframework.zip",
+      checksum: "3247cb5e1467b4cc713e1b4c71d9e5440db10f530e54093290536ad72c40acfe"
     )
   ]
 )
