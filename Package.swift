6243e062991ac45be9af0617de1595e56e71338e
GitHub Action <action@github.com>
2025-10-14 04:58:16 +0000

    Update ConvoUI for convoui-v30e9500-swift6_0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "ConvoUI",
-      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/convoui-vbac5ae1-swift6_0/ConvoUI.xcframework.zip",
-      checksum: "0a6dd1c3d7a9c8456728ff6db75dbbcd4c7f979954714f163debc6a3a268d58c"
+      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/convoui-v30e9500-swift6_0/ConvoUI.xcframework.zip",
+      checksum: "8f7e222302e43287484b1a7b8bb6bb02bd1c8a01cb13731968a660bc2d706595"
     )
   ]
 )
