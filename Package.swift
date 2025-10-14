4653072187b6bcbde78c59d6c69bdb97960a9afc
eleung <=>
2025-10-14 20:34:19 +0800

    Add ConvoUI to deps-bootstrap manifest

--- Package.swift
+++ Package.swift
@@ -3,12 +3,14 @@ import PackageDescription
 
 // Bootstrap manifest for neuronkit builds - contains only dependencies (no NeuronKit)
 // This prevents circular dependency when neuronkit tries to build using finclip-neuron
+// Auto-synced from main-swift6_0 by sync-bootstrap workflow
 let package = Package(
   name: "finclip-neuron",
   platforms: [.iOS(.v14), .macOS(.v12)],
   products: [
     .library(name: "SandboxSDK", targets: ["SandboxSDK"]),
-    .library(name: "convstorelib", targets: ["convstorelib"])
+    .library(name: "convstorelib", targets: ["convstorelib"]),
+    .library(name: "ConvoUI", targets: ["ConvoUI"])
   ],
   targets: [
     .binaryTarget(
@@ -20,6 +22,11 @@ let package = Package(
       name: "convstorelib",
       url: "https://github.com/Geeksfino/finclip-neuron/releases/download/conv-v1a27ae4-swift6_0/convstorelib.xcframework.zip",
       checksum: "e140e4dff2461e3d7cb8270c3c2204e75ab4d18a64cad6604eb34b8c04dd06bd"
+    ),
+    .binaryTarget(
+      name: "ConvoUI",
+      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/convoui-vb2f5e8a-swift6_0/ConvoUI.xcframework.zip",
+      checksum: "21e8570e8d58547755babd1f9c70d0b1228b60091abf8e61e1199a5b4a996c11"
     )
   ]
 )
