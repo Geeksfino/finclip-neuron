4ee1bb16047170b3ee8ea1da764bb3e51988edbf
GitHub Action <action@github.com>
2025-10-14 10:34:25 +0000

    Update SandboxSDK for sdk-vf64da07-swift6_0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     .binaryTarget(
       name: "SandboxSDK",
       url: "https://github.com/Geeksfino/finclip-neuron/releases/download/sdk-vf64da07-swift6_0/SandboxSDK.xcframework.zip",
-      checksum: "28ead8a0e07b2f0c6e1155090b060f7c313f9253466fa52c9fec382579851afc"
+      checksum: "95dcfa3bb8584e93c9d8b2297f67ba71f1b71458e9b8cdec9e6f06734e2dc973"
     )
    ,
     .binaryTarget(
