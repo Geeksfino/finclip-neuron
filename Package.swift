83a31604e670bd9ab8e3d86c64b82ee5e47aeebd
eleung <=>
2025-10-14 14:15:15 +0800

    Remove macOS platform since ConvoUI framework is added, which is only for iOS

--- Package.swift
+++ Package.swift
@@ -3,18 +3,27 @@ import PackageDescription
 
 let package = Package(
   name: "finclip-neuron",
-  platforms: [
-    .iOS(.v14),
-    .macOS(.v12)
-  ],
+  platforms: [.iOS(.v14)],
   products: [
-    .library(name: "ConvoUI", targets: ["ConvoUI"])
+    .library(name: "SandboxSDK", targets: ["SandboxSDK"]),
+    .library(name: "convstorelib", targets: ["convstorelib"]),
+    .library(name: "NeuronKit", targets: ["NeuronKit"])
   ],
   targets: [
     .binaryTarget(
-      name: "ConvoUI",
-      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/convoui-v30e9500-swift6_0/ConvoUI.xcframework.zip",
-      checksum: "8f7e222302e43287484b1a7b8bb6bb02bd1c8a01cb13731968a660bc2d706595"
+      name: "SandboxSDK",
+      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/sdk-vf64da07-swift6_0/SandboxSDK.xcframework.zip",
+      checksum: "ca5db62a860bb421bdefe6ceed26bdb8efa00789f65e838ddd2e3508e277cbbc"
+    ),
+    .binaryTarget(
+      name: "convstorelib",
+      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/conv-v1a27ae4-swift6_0/convstorelib.xcframework.zip",
+      checksum: "bdf13010debfd9463d1fd14f32861554f99fb86e503eaac5ea6fe0c962a07d66"
+    ),
+    .binaryTarget(
+      name: "NeuronKit",
+      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/neuronkit-v1dce083-swift6_0/NeuronKit.xcframework.zip",
+      checksum: "b87dcdf1aa8d4f516fb41a5c0b6c3bd8714d6f9fd8c1b22a9bbe82b1d3df3695"
     )
   ]
 )
