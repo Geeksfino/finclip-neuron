6c7a971bbd33f2f5742c61d7dcb8957187e00417
eleung <=>
2025-10-14 19:52:21 +0800

    Add deps-bootstrap manifest without NeuronKit for neuronkit builds

--- Package.swift
+++ Package.swift
@@ -1,29 +1,25 @@
 // swift-tools-version: 6.0
 import PackageDescription
 
+// Bootstrap manifest for neuronkit builds - contains only dependencies (no NeuronKit)
+// This prevents circular dependency when neuronkit tries to build using finclip-neuron
 let package = Package(
   name: "finclip-neuron",
   platforms: [.iOS(.v14), .macOS(.v12)],
   products: [
     .library(name: "SandboxSDK", targets: ["SandboxSDK"]),
-    .library(name: "convstorelib", targets: ["convstorelib"]),
-    .library(name: "NeuronKit", targets: ["NeuronKit"])
+    .library(name: "convstorelib", targets: ["convstorelib"])
   ],
   targets: [
     .binaryTarget(
       name: "SandboxSDK",
       url: "https://github.com/Geeksfino/finclip-neuron/releases/download/sdk-vf64da07-swift6_0/SandboxSDK.xcframework.zip",
-      checksum: "ca5db62a860bb421bdefe6ceed26bdb8efa00789f65e838ddd2e3508e277cbbc"
+      checksum: "95dcfa3bb8584e93c9d8b2297f67ba71f1b71458e9b8cdec9e6f06734e2dc973"
     ),
     .binaryTarget(
       name: "convstorelib",
       url: "https://github.com/Geeksfino/finclip-neuron/releases/download/conv-v1a27ae4-swift6_0/convstorelib.xcframework.zip",
-      checksum: "bdf13010debfd9463d1fd14f32861554f99fb86e503eaac5ea6fe0c962a07d66"
-    ),
-    .binaryTarget(
-      name: "NeuronKit",
-      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/neuronkit-v1dce083-swift6_0/NeuronKit.xcframework.zip",
-      checksum: "b87dcdf1aa8d4f516fb41a5c0b6c3bd8714d6f9fd8c1b22a9bbe82b1d3df3695"
+      checksum: "e140e4dff2461e3d7cb8270c3c2204e75ab4d18a64cad6604eb34b8c04dd06bd"
     )
   ]
 )
