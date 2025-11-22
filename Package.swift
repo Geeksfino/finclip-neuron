385c910b45a27278277eeb669603c4d0bc16d273
GitHub Action <action@github.com>
2025-11-22 13:47:47 +0000

    Sync deps-bootstrap from main-swift6_0
    Updated dependencies:
    - SandboxSDK: https://github.com/Geeksfino/finclip-neuron/releases/download/sdk-v3087fd0-swift6_0/SandboxSDK.xcframework.zip
    - convstorelib: https://github.com/Geeksfino/finclip-neuron/releases/download/conv-v59f911a-swift6_0/convstorelib.xcframework.zip
    - ConvoUI: https://github.com/Geeksfino/finclip-neuron/releases/download/convoui-v10fc8fe-swift6_0/ConvoUI.xcframework.zip

--- Package.swift
+++ Package.swift
@@ -25,8 +25,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "ConvoUI",
-      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/convoui-vab4ce4a-swift6_0/ConvoUI.xcframework.zip",
-      checksum: "09b8d7ee7f3e417678466cdbb8ea7910bf5a06b1132102a82de28fa3dd582d0c"
+      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/convoui-v10fc8fe-swift6_0/ConvoUI.xcframework.zip",
+      checksum: "b3773ef8d99e167016fdeb87b40f46c74061813b3776e531204a558aeffedd51"
     )
   ]
 )
