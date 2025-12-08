d608365f1d47e3c73c1889d2d5ee7ad4c567c515
GitHub Action <action@github.com>
2025-12-08 04:38:46 +0000

    Update SandboxSDK for sdk-v4aef8eb-swift6_0

--- Package.swift
+++ Package.swift
@@ -3,10 +3,7 @@ import PackageDescription
 
 let package = Package(
   name: "finclip-neuron",
-  platforms: [
-    .iOS(.v14),
-    .macOS(.v12)
-  ],
+  platforms: [.iOS(.v14), .macOS(.v12)],
   products: [
     .library(name: "SandboxSDK", targets: ["SandboxSDK"]),
     .library(name: "convstorelib", targets: ["convstorelib"]),
@@ -16,9 +13,10 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "SandboxSDK",
-      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/sdk-vb922bba-swift6_0/SandboxSDK.xcframework.zip",
-      checksum: "a988a8e757ed9b5a639bffecf45fc146a0c6ac160552ddc4deaf0bf6766e5964"
-    ),
+      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/sdk-v4aef8eb-swift6_0/SandboxSDK.xcframework.zip",
+      checksum: "1ec9c58742c018a1e4a356e3d51c8b34717978f5b7e8d2052815ecd172895416"
+    )
+   ,
     .binaryTarget(
       name: "convstorelib",
       url: "https://github.com/Geeksfino/finclip-neuron/releases/download/conv-va8eb9b5-swift6_0/convstorelib.xcframework.zip",
