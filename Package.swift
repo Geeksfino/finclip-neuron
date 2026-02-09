def45c3b62e44db199e79809db432b269632a677
GitHub Action <action@github.com>
2026-02-09 07:20:14 +0000

    Update convstorelib for conv-vfb7b6f0-swift6_0

--- Package.swift
+++ Package.swift
@@ -3,12 +3,15 @@ import PackageDescription
 
 let package = Package(
   name: "finclip-neuron",
-  platforms: [.iOS(.v14), .macOS(.v12)],
+  platforms: [
+    .iOS(.v14),
+    .macOS(.v12)
+  ],
   products: [
     .library(name: "SandboxSDK", targets: ["SandboxSDK"]),
     .library(name: "convstorelib", targets: ["convstorelib"]),
-    .library(name: "ConvoUI", targets: ["ConvoUI"]),
-    .library(name: "NeuronKit", targets: ["NeuronKit"])
+    .library(name: "NeuronKit", targets: ["NeuronKit"]),
+    .library(name: "ConvoUI", targets: ["ConvoUI"])
   ],
   targets: [
     .binaryTarget(
@@ -18,18 +21,20 @@ let package = Package(
     ),
     .binaryTarget(
       name: "convstorelib",
-      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/conv-v0d422c1-swift6_0/convstorelib.xcframework.zip",
-      checksum: "80ef958c713df6a361246ccd53fa4f679ab5892d8999dcd7f6016b08426fefbe"
-    ),
-    .binaryTarget(
-      name: "ConvoUI",
-      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/convoui-vd2c0a71-swift6_0/ConvoUI.xcframework.zip",
-      checksum: "770cf1bc3e5cdb7325376ee5b17e99a7b98fedde2ee5637e0a1cacbab81d10e5"
-    ),
+      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/conv-vfb7b6f0-swift6_0/convstorelib.xcframework.zip",
+      checksum: "d532a0a938d447875b8b8bee4795198d93a93a368c957b9461290fa1f1773213"
+    )
+   ,
     .binaryTarget(
       name: "NeuronKit",
       url: "https://github.com/Geeksfino/finclip-neuron/releases/download/neuronkit-vdc976d2-swift6_0/NeuronKit.xcframework.zip",
       checksum: "064a2bf6088067b3c9b119c0fbe9252889c8ce8987acb1be6b83a0eb88416afa"
+    )
+   ,
+    .binaryTarget(
+      name: "ConvoUI",
+      url: "https://github.com/Geeksfino/finclip-neuron/releases/download/convoui-vd2c0a71-swift6_0/ConvoUI.xcframework.zip",
+      checksum: "770cf1bc3e5cdb7325376ee5b17e99a7b98fedde2ee5637e0a1cacbab81d10e5"
     )
   ]
 )
